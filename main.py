b6dcfad3bb92e2c6f67fddc29213618c5471c30e
dsk9278 <228854832+dsk9278@ users.noreply.github.com>
2025-11-22 00:34:25 +0900

    CI/CDツール、Ruffの追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from mt_signal_search.app import SignalTraceApplication
 
+
 def main():
     app = SignalTraceApplication()
     return app.run()
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
